ff9a2250e2ffbf50d79c082c12262b16988dbbb1
DiegoDelgado82 <diegodelgado365@gmail.com>
2022-11-17 10:45:53 -0300

    17-11 4

---
+++ js/dataStore.js
@@ -0,0 +1,37 @@
+async function getStore() {
+    let url = 'https://raw.githubusercontent.com/DiegoDelgado82/serviciotecnico/main/tiendas.json';
+               
+    try {
+        let res = await fetch(url);
+        return await res.json();
+        
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  async function datosTienda(nro) {
+    
+    let tiendas = await getStore();
+    let leyenda="<h3>"+nro+"</h3><br>";
+    var xIndex=0;
+    var stores=[];
+    var provinces=[];
+
+    tiendas.forEach(tiendas => {
+    if (tiendas.Nro===nro)
+        {
+            stores[xIndex]=tiendas.Nombre;
+            alert("Nro tienda: "+ tiendas.Nro + " Nombre: "+tiendas.Nombre+ " Direccion: " + tiendas.Direccion + " Telefono: "+tiendas.Telefono)  
+            xIndex++;
+        }
+
+    
+      
+    });
+    document.getElementById("datostienda").innerHTML =leyenda+ "</ul><br><br><br><br><h4>Datos de la tienda</h4>"
+    
+  
+  }
+
+  
--- js/store.js
+++ js/store.js
@@ -11,9 +11,9 @@ async function getStore() {
   }
 
   async function renderStore(sto) {
-    
+    document.getElementById("datosTiendaMapa").innerHTML=""
     let tiendas = await getStore();
-    let leyenda="<h3>"+sto+"</h3><ul>";
+    let leyenda="<h3>"+sto+"</h3><br>";
     var xIndex=0;
     var stores=[];
     var provinces=[];
@@ -22,17 +22,46 @@ async function getStore() {
     if (tiendas.Provincia===sto)
         {
             stores[xIndex]=tiendas.Nombre;
-            leyenda= leyenda+'<li class="listStore">'+stores[xIndex]+'</li> '  
+            leyenda= leyenda+'<li  onclick=datosTienda('+tiendas.Nro+') class="listStore">'+stores[xIndex]+'</li> '  
             xIndex++;
         }
 
     
       
     });
-    document.getElementById("demo").innerHTML =leyenda
+    document.getElementById("demo").innerHTML =leyenda+ "</ul>"
     
   
   }
   
   //renderStore();
   
+ 
+  async function datosTienda(nro) {
+    
+    let tiendas = await getStore();
+    let leyenda="<h3>"+nro+"</h3><br>";
+    var xIndex=0;
+    var stores=[];
+    var provinces=[];
+
+    tiendas.forEach(tiendas => {
+       
+    if (Number(tiendas.Nro)===nro)
+        {
+            stores[xIndex]=tiendas.Nombre;
+            xIndex++;
+            document.getElementById("datosTiendaMapa").innerHTML= "<h4>Datos de la tienda</h4><ul><li>Dirección: "+tiendas.Direccion+ 
+            "</li><li>Provincia: "+ tiendas.Provincia+"</li><li>Localidad: A completar </li><li>Telefono: "+tiendas.Telefono + 
+            "</li><li>Líder de Tienda: A completar </li> </ul>";
+            
+
+        }
+
+
+    
+      
+    });
+    
+  
+  }
