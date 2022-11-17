9af5634379c930bbabdb9b8a8f535b7fc2790970
DiegoDelgado82 <diegodelgado365@gmail.com>
2022-11-17 11:18:00 -0300

    17-11 5

--- js/store.js
+++ js/store.js
@@ -51,9 +51,9 @@ async function getStore() {
         {
             stores[xIndex]=tiendas.Nombre;
             xIndex++;
-            document.getElementById("datosTiendaMapa").innerHTML= "<h4>Datos de la tienda</h4><ul><li>Dirección: "+tiendas.Direccion+ 
-            "</li><li>Provincia: "+ tiendas.Provincia+"</li><li>Localidad: A completar </li><li>Telefono: "+tiendas.Telefono + 
-            "</li><li>Líder de Tienda: A completar </li> </ul>";
+            document.getElementById("datosTiendaMapa").innerHTML= "<h4>Datos de la tienda "+tiendas.Nro+"-"+tiendas.Nombre+"</h4><ul><li>Dirección: "+tiendas.Direccion+ 
+            "</li><li>Provincia: "+ tiendas.Provincia+"</li><li>Localidad:"+tiendas.Localidad+"</li><li>Telefono: "+tiendas.Telefono + 
+            "</li><li>Líder de Tienda: "+tiendas.Lider+" </li> </ul>";
             
 
         }
