295aae6319f2debae1ef5314a9b9e991ef06b0db
DiegoDelgado82 <diegodelgado365@gmail.com>
2023-04-04 11:25:42 -0300

    04-04 7

--- js/scanner.js
+++ js/scanner.js
@@ -1,4 +1,6 @@
 
+let ean;
+
 
 async function getCuota() {
     //let url = './scr/garantia.json';
@@ -8,9 +10,11 @@ async function getCuota() {
         let res = await fetch(url);
         return await res.json();
         
+        
     } catch (error) {
         console.log(error);
     }
+    alert("cargo ok");
   }
 
 
@@ -72,27 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 
 	Quagga.onProcessed(function (result) {
-		var drawingCtx = Quagga.canvas.ctx.overlay,
-			drawingCanvas = Quagga.canvas.dom.overlay;
-
-		if (result) {
-			if (result.boxes) {
-				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-				result.boxes.filter(function (box) {
-					return box !== result.box;
-				}).forEach(function (box) {
-					Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-				});
-			}
-
-			if (result.box) {
-				Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-			}
-
-			if (result.codeResult && result.codeResult.code) {
-				Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-			}
-		}
+		
 	});
 });
 
